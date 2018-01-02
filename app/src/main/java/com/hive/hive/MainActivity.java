cc1082a94b03585beb25f554ee86fbc07e9d9283
Vítor Resing Plentz <vrplentz@inf.ufpel.edu.br>
2018-01-02 16:12:53 -0200

    BottomNavigationViewHelper used to make BottomNavigatorView show more than 3 items; MainActivity calling BottomNavigationViewHelper; nav_item_background.xml selector created to draw line under item selected item; nav_item_color_state.xml selector created to change color os selected itens; activity_bottom_tab.xml using BottomNavigationView and ViewPager; menu_bottom_tab.xml with tab items; colors.xml added some colors; string.xml added some strings; styles.xml with app colors

--- app/src/main/java/com/hive/hive/MainActivity.java
+++ app/src/main/java/com/hive/hive/MainActivity.java
@@ -1,13 +1,38 @@
 package com.hive.hive;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.hive.hive.helpers.BottomNavigationViewHelper;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView mBottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bottom_tab);
+        mBottomNavigationView = findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.disableShiftMode(mBottomNavigationView);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_home:
+                        
+                    case R.id.action_feed:
+
+                    case R.id.action_association:
+
+                    case R.id.action_shop:
+
+                    case R.id.action_map:
+
+                }
+                return  true;
+            }
+        });
     }
 }
