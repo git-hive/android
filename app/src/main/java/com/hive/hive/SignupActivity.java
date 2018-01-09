4ea737ecd306881294ff2b688541e9dd2535a1bb
mabirck <mafbirck@inf.ufpel.edu.br>
2018-01-09 12:07:21 -0200

    incorporate dummy signup screen(I forgot), polish signin and signup with nasty but useful scrollview.

---
+++ app/src/main/java/com/hive/hive/SignupActivity.java
@@ -0,0 +1,48 @@
+package com.hive.hive;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        //Getting Views and Buttons References
+        Button mCompleteSignUp = findViewById(R.id.mCompleteSignUp);
+        //----------------------------------//
+
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }else{
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            // Remember that you sho    uld never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+            actionBar.hide();
+        }
+
+        mCompleteSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = SignupActivity.this;
+                String textToShow = "DATABASE CONNECTION NOT IMPLEMENTED YET!";
+                Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
