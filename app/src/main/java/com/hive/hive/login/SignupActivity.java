43e5e0c042de04aef97089b637d4eb7f9adfc6a3
João Fernandes <joaovitorfernandes2@gmail.com>
2018-01-21 17:16:32 -0200

    Add check for null value on getSupportActionBar call

--- app/src/main/java/com/hive/hive/login/SignupActivity.java
+++ app/src/main/java/com/hive/hive/login/SignupActivity.java
@@ -31,10 +31,12 @@ public class SignupActivity extends AppCompatActivity {
             // Hide the status bar.
             int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
             decorView.setSystemUiVisibility(uiOptions);
-            // Remember that you sho    uld never show the action bar if the
+            // Remember that you should never show the action bar if the
             // status bar is hidden, so hide that too if necessary.
             android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-            actionBar.hide();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
         }
 
         mCompleteSignUp.setOnClickListener(new View.OnClickListener() {
