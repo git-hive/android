2b4051019d82e143c572960613c4e9e5740136db
Nícolas Oreques de Araujo <nicolas.de.araujo@hotmail.com>
2018-01-29 01:56:53 -0200

    Basic attributes and relationships of model/forum

--- app/src/main/java/com/hive/hive/model/forum/ForumComment.java
+++ app/src/main/java/com/hive/hive/model/forum/ForumComment.java
@@ -1,8 +1,71 @@
 package com.hive.hive.model.forum;
 
+import com.hive.hive.model.marketplace.PointsTransaction;
+
+import java.util.ArrayList;
+
 /**
  * Created by naraujo on 1/28/18.
  */
 
 public class ForumComment extends ForumAction {
+
+    private String content;
+    private int supportScore;
+
+    private String postId;
+
+    private ArrayList<String> supportIds;
+
+    //--- Constructor
+
+    public ForumComment(
+            String id, String authorId, PointsTransaction pointsTransaction, String forumId,
+            String content, int supportScore, String postId, ArrayList<String> supportIds)
+    {
+        super(id, authorId, pointsTransaction, forumId);
+        this.content = content;
+        this.supportScore = supportScore;
+        this.postId = postId;
+        this.supportIds = supportIds;
+    }
+
+
+    //--- Getters
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public int getSupportScore() {
+
+
+        return supportScore;
+    }
+
+    public ArrayList<String> getSupportIds() {
+        return supportIds;
+    }
+
+    //--- Setters
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public void setSupportScore(int supportScore) {
+        this.supportScore = supportScore;
+    }
+
+    public void setSupportIds(ArrayList<String> supportIds) {
+        this.supportIds = supportIds;
+    }
 }
--- app/src/main/java/com/hive/hive/model/user/UserAction.java
+++ app/src/main/java/com/hive/hive/model/user/UserAction.java
@@ -9,12 +9,19 @@ import com.hive.hive.model.marketplace.PointsTransaction;
 public abstract class UserAction {
 
     private String id;
+    private long createdAt;
+    private long updatedAt;
+    private String authorId;
     private PointsTransaction pointsTransaction;
 
 
+
     //--- Constructor
-    public UserAction(String id, PointsTransaction pointsTransaction) {
+    public UserAction(String id, String authorId, PointsTransaction pointsTransaction) {
         this.id = id;
+        this.createdAt = System.currentTimeMillis();
+        this.updatedAt = System.currentTimeMillis();
+        this.authorId = authorId;
         this.pointsTransaction = pointsTransaction;
     }
 
@@ -22,6 +29,18 @@ public abstract class UserAction {
 
     public String getId() {
         return id;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getAuthorId() {
+        return authorId;
     }
 
     public PointsTransaction getPointsTransaction() {
@@ -33,6 +52,18 @@ public abstract class UserAction {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
     }
 
     public void setPointsTransaction(PointsTransaction pointsTransaction) {
