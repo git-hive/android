6623666b4a2d58096410ffa1fa734e09cdfd81e5
Nícolas Oreques de Araujo <nicolas.de.araujo@hotmail.com>
2018-01-29 23:08:26 -0200

    Basic attribute and relationships of model/marketplace

--- app/src/main/java/com/hive/hive/model/marketplace/PointsTransaction.java
+++ app/src/main/java/com/hive/hive/model/marketplace/PointsTransaction.java
@@ -6,5 +6,77 @@ import com.hive.hive.model.user.UserAction;
  * Created by naraujo on 1/28/18.
  */
 
-public class PointsTransaction extends UserAction {
+public class PointsTransaction {
+
+    private String id;
+    private long createdAt;
+    private long updatedAt;
+    private int value;
+
+    private String userActionId;    //Associated user action that is the reason of the transaction
+    private String walletId;        //Associated wallet where the transactions acts
+
+
+    //--- Constructors
+
+    public PointsTransaction(String id, long createdAt, long updatedAt, int value, String walletId, String userActionId) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.value = value;
+        this.walletId = walletId;
+        this.userActionId = userActionId;
+    }
+
+    //--- Getters
+
+    public String getId() {
+        return id;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getWalletId() {
+        return walletId;
+    }
+
+    public String getUserActionId() {
+        return userActionId;
+    }
+
+    //--- Setters
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setWalletId(String walletId) {
+        this.walletId = walletId;
+    }
+
+    public void setUserActionId(String userActionId) {
+        this.userActionId = userActionId;
+    }
 }
--- app/src/main/java/com/hive/hive/model/marketplace/Product.java
+++ app/src/main/java/com/hive/hive/model/marketplace/Product.java
@@ -1,8 +1,72 @@
 package com.hive.hive.model.marketplace;
 
+import java.util.ArrayList;
+
 /**
  * Created by naraujo on 1/28/18.
  */
 
 public class Product {
+
+    private String id;
+    private String name;
+    private String description;
+
+    private String partnerId;
+    private ArrayList<String> categoriesIds;
+
+    //--- Constructor
+
+    public Product(String id, String name, String description, String partnerId, ArrayList<String> categoriesIds) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.partnerId = partnerId;
+        this.categoriesIds = categoriesIds;
+    }
+
+    //--- Getters
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPartnerId() {
+        return partnerId;
+    }
+
+    public ArrayList<String> getCategoriesIds() {
+        return categoriesIds;
+    }
+
+
+    //--- Setters
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPartnerId(String partnerId) {
+        this.partnerId = partnerId;
+    }
+
+    public void setCategoriesIds(ArrayList<String> categoriesIds) {
+        this.categoriesIds = categoriesIds;
+    }
 }
