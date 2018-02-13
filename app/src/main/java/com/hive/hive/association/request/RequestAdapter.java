5cb3124dea4a54ce3ac0d7991b00f59ad78270bf
Nícolas Oreques de Araujo <nicolas.de.araujo@hotmail.com>
2018-02-13 04:00:36 -0200

    ViewHolder + Adapter + ItemLayout for Requests

--- app/src/main/java/com/hive/hive/association/request/RequestAdapter.java
+++ app/src/main/java/com/hive/hive/association/request/RequestAdapter.java
@@ -4,9 +4,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.hive.hive.R;
-import com.hive.hive.home.RequestViewHolder;
+import com.hive.hive.home.RequestViewHolderOld;
 import com.hive.hive.model.association.Request;
 
 import java.util.ArrayList;
@@ -16,7 +18,7 @@ import java.util.HashMap;
  * Created by vplentz on 04/02/18.
  */
 
-public class RequestAdapter extends RecyclerView.Adapter<RequestViewHolder> {
+public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.RequestViewHolder> {
 
     //-- Data
     private HashMap<String,  Request> mRequests;
@@ -34,13 +36,65 @@ public class RequestAdapter extends RecyclerView.Adapter<RequestViewHolder> {
 
     @Override
     public void onBindViewHolder(RequestViewHolder holder, int position) {
-        String id = mIds.get(position);
-        holder.getReqTitleTV().setText(mRequests.get(id).getTitle());
-        holder.getReqMessageTV().setText(mRequests.get(id).getContent());
+
+        Request request = mRequests.get(mIds.get(position));
+
+        holder.mItem = request;
+
+        holder.mUserAvatar.setImageResource(R.drawable.ic_profile_photo);
+        holder.mUserName.setText("Vitor Plantas");
+        holder.mRequestTitle.setText(request.getTitle());
+        holder.mRequestContent.setText(request.getContent());
+        holder.mNumberOfSupports.setText(String.valueOf(request.getScore()));
+
 
     }
     @Override
     public int getItemCount() {
         return mRequests.size();
     }
+
+
+    /**
+     * Class to serve as ViewHolder for a Request model in this adapter
+     */
+    class RequestViewHolder extends RecyclerView.ViewHolder{
+
+        final View mView;
+
+        final ImageView mUserAvatar;
+        final TextView mUserName;
+        final TextView mUserLeaderboardPostion;
+        final TextView mRequestTitle;
+        final TextView mRequestCost;
+        final ImageView mRequestCategory;
+        final TextView mRequestContent;
+        final TextView mNumberOfComments;
+        final TextView mNumberOfSupports;
+
+        Request mItem;
+
+        RequestViewHolder(View view){
+            super(view);
+
+            mView = view;
+
+            mUserAvatar = view.findViewById(R.id.request_author_photo_iv);
+            mUserName = view.findViewById(R.id.request_author_name_tv);
+            mUserLeaderboardPostion = view.findViewById(R.id.request_leaderboard_position_tv);
+            mRequestTitle = view.findViewById(R.id.request_title_tv);
+            mRequestCost = view.findViewById(R.id.request_cost_tv);
+            mRequestCategory = view.findViewById(R.id.request_budget_category_iv);
+            mRequestContent = view.findViewById(R.id.request_content_tv);
+            mNumberOfComments = view.findViewById(R.id.request_number_of_comments_tv);
+            mNumberOfSupports = view.findViewById(R.id.request_number_of_supports_tv);
+
+        }
+
+        @Override
+        public String toString(){
+            return "";
+        }
+    }
+
 }
