ead93def49c3f356799242fc5726d795d9280290
Marco Birck <mafbirck@gmail.com>
2018-03-16 15:54:48 -0300

    Add primitive storyline on top of questions

---
+++ app/src/main/java/com/hive/hive/association/votes/tabs/questions/model/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.hive.hive.association.votes.tabs.questions.model;
+
+/**
+ * Created by HP-HP on 07-06-2016.
+ */
+public enum OrderStatus {
+
+    COMPLETED,
+    ACTIVE,
+    INACTIVE;
+
+}
