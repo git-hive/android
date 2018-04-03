f4b981a9fdb8235c67fe787741a4557996787348
Nícolas Oreques de Araujo <nicolas.de.araujo@hotmail.com>
2018-04-02 23:51:53 -0300

    Updates in base transparency activity
    Updated strings, refactored code to attende to project standards

--- app/src/main/java/com/hive/hive/association/transparency/TransparencyActivity.java
+++ app/src/main/java/com/hive/hive/association/transparency/TransparencyActivity.java
@@ -4,8 +4,6 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.view.View;
@@ -14,17 +12,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hive.hive.R;
-import com.hive.hive.association.transparency.TransparencyFragmentPagerAdapter;
 
 public class TransparencyActivity extends AppCompatActivity {
     // Superior Tab items
-    TextView mHeaderAssembleia;
-    ImageView mExitVotesIV;
-    ImageView mSearchVotesIV;
+    TextView mTitleTV;
+    ImageView mUpButtonIV;
+    ImageView mSearchIV;
 
     // Tab components
-    TabLayout tabLayout;
-    ViewPager viewPager;
+    TabLayout transparencyTL;
+    ViewPager transparencyVP;
 
 
 
@@ -33,31 +30,31 @@ public class TransparencyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_votes);
+        setContentView(R.layout.activity_transparency);
 
-        mHeaderAssembleia = findViewById(R.id.assembleiaTV);
-        mExitVotesIV = findViewById(R.id.exitVotesIV);
-        mSearchVotesIV = findViewById(R.id.searchVotesIV);
+        mTitleTV = findViewById(R.id.transparency_activity_title_TV);
+        mUpButtonIV = findViewById(R.id.up_button_transparency_IV);
+        mSearchIV = findViewById(R.id.search_transparency_IV);
 
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new TransparencyFragmentPagerAdapter(getSupportFragmentManager(),
+        transparencyVP = (ViewPager) findViewById(R.id.transparency_VP);
+        transparencyVP.setAdapter(new TransparencyFragmentPagerAdapter(getSupportFragmentManager(),
                 TransparencyActivity.this));
 
         // Give the TabLayout the ViewPager
-        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        transparencyTL = (TabLayout) findViewById(R.id.sliding_tabs_transparency_TL);
+        transparencyTL.setupWithViewPager(transparencyVP);
 
         // Exit from activity
-        mExitVotesIV.setOnClickListener(new View.OnClickListener() {
+        mUpButtonIV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
 
-        mSearchVotesIV.setOnClickListener(new View.OnClickListener() {
+        mSearchIV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(TransparencyActivity.this, "Search not implemented yet.",
@@ -70,8 +67,8 @@ public class TransparencyActivity extends AppCompatActivity {
     }
 
     public void styleThings(){
-        SpannableString content = new SpannableString(getResources().getString(R.string.assembleia));
+        SpannableString content = new SpannableString(getResources().getString(R.string.transparency_activity_title));
         content.setSpan(new UnderlineSpan(), 0+1, content.length()-1, 0);
-        mHeaderAssembleia.setText(content);
+        mTitleTV.setText(content);
     }
 }
