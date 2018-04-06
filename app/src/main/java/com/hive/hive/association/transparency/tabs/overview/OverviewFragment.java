92fac757bae17aff5290c14ef3a6f33024da1c84
Nícolas Oreques de Araujo <nicolas.de.araujo@hotmail.com>
2018-04-06 00:58:37 -0300

    Transparency Monthly Balance Chart
    BarChart for monthly balance with randomly generated data (with realistic values).
    Chart formatting and labeling.

--- app/src/main/java/com/hive/hive/association/transparency/tabs/overview/OverviewFragment.java
+++ app/src/main/java/com/hive/hive/association/transparency/tabs/overview/OverviewFragment.java
@@ -2,8 +2,10 @@ package com.hive.hive.association.transparency.tabs.overview;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,8 @@ import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
@@ -19,6 +23,7 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.hive.hive.R;
 import com.hive.hive.association.transparency.TransparencyActivity;
@@ -43,11 +48,12 @@ public class OverviewFragment extends Fragment {
     private TransparencyActivity mActivity;
 
     //--- Data
-    private List<BudgetTransaction> transactions;
+    private List<BudgetTransaction> incomes;
+    private List<BudgetTransaction> expenses;
 
 
     //--- Chart
-    private BarChart monthlyChart;
+    private BarChart mMonthlyChart;
 
 
 
@@ -68,7 +74,8 @@ public class OverviewFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        transactions = new ArrayList<>();
+        incomes = new ArrayList<>();
+        expenses = new ArrayList<>();
         fillDummyContent();
 
 
@@ -84,41 +91,112 @@ public class OverviewFragment extends Fragment {
         mActivity = (TransparencyActivity) getActivity();
 
         //--- Views
-        monthlyChart = view.findViewById(R.id.monthly_chart);
+        mMonthlyChart = view.findViewById(R.id.monthly_chart);
 
-        //--- Chart Data
-        List<BarEntry> monthlyBudgets = new ArrayList<>();
-        Calendar calendar = Calendar.getInstance();
 
-        for(BudgetTransaction transaction : transactions){
-            calendar.setTimeInMillis(transaction.getDate());
-            monthlyBudgets.add(new BarEntry(calendar.get(Calendar.MONTH), transaction.getValue()));
-        }
+        //------------------------------------ Monthly Chart ------------------------------------//
 
-        //--- Chart Formatting
-        BarDataSet barDataSet = new BarDataSet(monthlyBudgets, "Orçamento Mensal");
+        //--- Formatting Parameters
+        float groupSpace = 0.2f;
+        float barSpace = 0.03f;
+        float barWidth = 0.02f;
+        float monthsToShow = 12;
+
+        //--------- Chart Data ---------
+        //-- Instantiate arrays of BarEntries
+        ArrayList<BarEntry> incomesEntries = new ArrayList<>();
+        ArrayList<BarEntry> expensesEntries = new ArrayList<>();
+
+        //-- Fill Incomes Entries
+        for (int i = 0; i < monthsToShow && i < incomes.size(); i++)
+            incomesEntries.add(new BarEntry(i, (int) incomes.get(i).getValue()));
+
+        //-- Fill Expenses Entries
+        for (int i = 0; i < monthsToShow && i < expenses.size(); i++)
+            expensesEntries.add(new BarEntry(i, (int) expenses.get(i).getValue()));
+
+        //-- Creates Income Dataset with entries
+        BarDataSet incomeDataset = new BarDataSet(incomesEntries, "Receita");
+        incomeDataset.setColor(ContextCompat.getColor(this.getActivity(), R.color.green_text));
+
+        //-- Creates Expense Dataset with Entries
+        BarDataSet expensesDataset = new BarDataSet(expensesEntries, "Despesa");
+        expensesDataset.setColor(ContextCompat.getColor(this.getActivity(), R.color.red_text));
+
+        //-- Creates label formatter
+        final String[] monthLabels = {
+                "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"
+        };
+
+        IAxisValueFormatter axisValueFormatter = new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+
+                if (value < 0 || value > 22)
+                    return Float.toString(value);
+
+                int index = (int) Math.floor(value/2f);
+
+                return monthLabels[index];
+
+            }
+        };
+
+
+        //-- Combine Incomes and Expenses Dataset
         ArrayList<IBarDataSet> dataSets = new ArrayList<>();
-        dataSets.add(barDataSet);
-        dataSets.add(barDataSet);
+        dataSets.add(incomeDataset);
+        dataSets.add(expensesDataset);
+
+        //-- Create Chart data with combined datasets
         BarData barData = new BarData(dataSets);
 
-//        //Format Axis
-//        XAxis xAxis = monthlyChart.getXAxis();
-//        YAxis yAxis = monthlyChart.getAxisLeft();
-//
-//        xAxis.setAxisMinimum(0);
-//        xAxis.setAxisMaximum(12);
-//        xAxis.setGranularity(0.5f);
-//        xAxis.setCenterAxisLabels(true);
-//        xAxis.setDrawGridLines(false);
-//
-//        yAxis.setDrawGridLines(false);
-//        yAxis.setAxisMinimum(0.0f);
-//        yAxis.setAxisMaximum(50000f);
+        //-- Format Chart xAxis
+        XAxis xAxis = mMonthlyChart.getXAxis();
+        xAxis.setAxisMinimum(0f);
+        xAxis.setAxisMaximum(11f);
+        xAxis.setGranularity(1f);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setDrawGridLines(false);
+        xAxis.setValueFormatter(axisValueFormatter);
 
-        monthlyChart.setData(barData);
-//        monthlyChart.groupBars(0f, 0.2f, 0.03f);
-        monthlyChart.invalidate();
+        //-- Format yAxis
+        YAxis leftAxis = mMonthlyChart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMinimum(0f);
+
+        //-- Format chart legend
+        Legend mLegend = mMonthlyChart.getLegend();
+        mLegend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        mLegend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        mLegend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        mLegend.setDrawInside(false);
+        mLegend.setXEntrySpace(5);
+        mLegend.setYEntrySpace(5);
+        mLegend.setTextSize(15f);
+        mLegend.setTextColor(Color.BLACK);
+        mLegend.setFormSize(10f);
+        mLegend.setWordWrapEnabled(true);
+
+        //-- Config chart generic options
+        mMonthlyChart.setEnabled(false);
+        mMonthlyChart.setDescription(null);
+
+        //-- Sets chart data
+        mMonthlyChart.setData(barData);
+
+        //-- Sets chart formatting
+        mMonthlyChart.groupBars(0f, groupSpace, barSpace);
+        mMonthlyChart.getXAxis().setAxisMaximum(barData.getGroupWidth(groupSpace, barSpace) * 12);
+        mMonthlyChart.setVisibleXRangeMaximum(12);
+        mMonthlyChart.setDrawGridBackground(false);
+        mMonthlyChart.setHorizontalScrollBarEnabled(true);
+        mMonthlyChart.getAxisRight().setEnabled(false);
+
+        //-- Updates chart view
+        mMonthlyChart.invalidate();
+
 
 
 
@@ -151,7 +229,7 @@ public class OverviewFragment extends Fragment {
 
 
 
-        //Create dummy transactions with random values
+        //Create dummy revenues with random values
         Random rand = new Random();
         for (int i = 0; i < 12; i++){
             final int aux = i;
@@ -170,9 +248,32 @@ public class OverviewFragment extends Fragment {
                                 put(categories.get(aux%5).getId(), categories.get(aux*3%5));
 
                             }},
-                            rand.nextFloat()
+                            100000 + (10000 * rand.nextInt(5))
                     );
-            transactions.add(budgetTransaction);
+            incomes.add(budgetTransaction);
+        }
+
+        //Create dummy expenses with random values
+        for (int i = 0; i < 12; i++){
+            final int aux = i;
+            calendar.add(Calendar.MONTH, i);
+            BudgetTransaction budgetTransaction =
+                    new BudgetTransaction(
+                            "1",
+                            System.currentTimeMillis(),
+                            System.currentTimeMillis(),
+                            calendar.getTimeInMillis(),
+                            "aaaa",
+                            new HashMap<String, BudgetTransactionCategories>()
+                            {{
+                                put(categories.get(aux%5).getId(), categories.get(aux%5));
+                                put(categories.get(aux%5).getId(), categories.get(aux*2%5));
+                                put(categories.get(aux%5).getId(), categories.get(aux*3%5));
+
+                            }},
+                            100000 + (10000 * rand.nextInt(5))
+                    );
+            expenses.add(budgetTransaction);
         }
 
     }
