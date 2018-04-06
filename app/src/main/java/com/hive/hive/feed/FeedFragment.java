9531bd6b76e6f096b7ba619fd1fda7028be92b77
Marco Birck <mafbirck@gmail.com>
2018-04-06 19:54:47 -0300

    Populating ugly empty tabs, *Part 2

--- app/src/main/java/com/hive/hive/feed/FeedFragment.java
+++ app/src/main/java/com/hive/hive/feed/FeedFragment.java
@@ -4,16 +4,26 @@ package com.hive.hive.feed;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.hive.hive.R;
 import com.hive.hive.association.transparency.tabs.budget.BudgetFragment;
+import com.hive.hive.home.RecyclerViewHomeAdapter;
+import com.hive.hive.model.association.Request;
+import com.hive.hive.model.forum.ForumPost;
 
 import java.util.ArrayList;
 
 public class FeedFragment extends Fragment {
+
+    private RecyclerView mRecyclerViewHome;
+    private RecyclerViewHomeAdapter mRecyclerViewHomeAdapter;
+    ArrayList<Object> DUMMYARRAY;
+
 
     public FeedFragment() {
         // Required empty public constructor
@@ -30,14 +40,26 @@ public class FeedFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_feed, container, false);
+        DUMMYARRAY = new ArrayList<>();
+        DUMMYARRAY.add(new ForumPost());
+        DUMMYARRAY.add(new Request());
+        DUMMYARRAY.add(new ForumPost());
+        DUMMYARRAY.add(new Request());
+        DUMMYARRAY.add(new ForumPost());
+        DUMMYARRAY.add(new Request());
+        DUMMYARRAY.add(new ForumPost());
+        DUMMYARRAY.add(new Request());
+
+        mRecyclerViewHome = v.findViewById(R.id.recyclerViewFeed);
+        mRecyclerViewHomeAdapter = new RecyclerViewHomeAdapter(DUMMYARRAY);
+        mRecyclerViewHome.setAdapter(mRecyclerViewHomeAdapter);
+        mRecyclerViewHome.setLayoutManager(new LinearLayoutManager(v.getContext()));
+        return v;
     }
 
-    private void initializeData(){
-        budgets = new ArrayList<>();
-        budgets.add(new BudgetFragment.Budget("Praça Food-Truck", 0));
-        budgets.add(new BudgetFragment.Budget("Playground Holandês", 0));
-        budgets.add(new BudgetFragment.Budget("Quadra Poliesportiva", 0));
-    }
+
 
 }
