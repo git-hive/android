9e643b7446a65f9df57b891aa88fc8495deee7a9
Nícolas Oreques de Araujo <nicolas.de.araujo@hotmail.com>
2018-04-06 21:01:30 -0300

    Staff Base Layout Items

--- app/src/main/java/com/hive/hive/association/transparency/tabs/staff/StaffFragment.java
+++ app/src/main/java/com/hive/hive/association/transparency/tabs/staff/StaffFragment.java
@@ -24,13 +24,7 @@ public class StaffFragment extends Fragment {
     private static final String TAG = StaffFragment.class.getSimpleName();
     public static final String ARG_PAGE = "Funcionários";
 
-    //--- Views
-    ExpandableListAdapter listAdapter;
-    ExpandableListView staffExpLV;
 
-    //--- Data
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
 
     //-- Context
     private TransparencyActivity mActivity;
@@ -58,72 +52,6 @@ public class StaffFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_staff, container, false);
 
-        // get the listview
-        staffExpLV = (ExpandableListView) view.findViewById(R.id.staffExpandableLV);
-
-        // preparing list data
-        prepareListData();
-
-        listAdapter = new ExpandableListAdapter(getContext(), listDataHeader, listDataChild);
-
-        // setting list adapter
-        staffExpLV.setAdapter(listAdapter);
-
-        // Listview Group click listener
-        staffExpLV.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
-
-            @Override
-            public boolean onGroupClick(ExpandableListView parent, View v,
-                                        int groupPosition, long id) {
-                // Toast.makeText(getApplicationContext(),
-                // "Group Clicked " + listDataHeader.get(groupPosition),
-                // Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-
-        // Listview Group expanded listener
-        staffExpLV.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
-
-            @Override
-            public void onGroupExpand(int groupPosition) {
-                Toast.makeText(getContext(),
-                        listDataHeader.get(groupPosition) + " Expanded",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // Listview Group collasped listener
-        staffExpLV.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
-
-            @Override
-            public void onGroupCollapse(int groupPosition) {
-                Toast.makeText(getContext(),
-                        listDataHeader.get(groupPosition) + " Collapsed",
-                        Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        // Listview on child click listener
-        staffExpLV.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v,
-                                        int groupPosition, int childPosition, long id) {
-                // TODO Auto-generated method stub
-                Toast.makeText(
-                        getContext(),
-                        listDataHeader.get(groupPosition)
-                                + " : "
-                                + listDataChild.get(
-                                listDataHeader.get(groupPosition)).get(
-                                childPosition), Toast.LENGTH_SHORT)
-                        .show();
-                return false;
-            }
-        });
-
         mActivity = (TransparencyActivity) getActivity();
 
         return  view;
@@ -133,16 +61,6 @@ public class StaffFragment extends Fragment {
          * Preparing the list data
          */
     private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
-
-        // Adding child data
-        listDataHeader.add("Jardinagem");
-        listDataHeader.add("Limpeza");
-        listDataHeader.add("Segurança");
-        listDataHeader.add("Portaria");
-        listDataHeader.add("Serviços");
-        listDataHeader.add("Gerência");
 
         // Adding child data
         List<String> gardening = new ArrayList<String>();
@@ -189,13 +107,6 @@ public class StaffFragment extends Fragment {
         manager.add("The Spectacular Now");
         manager.add("The Canyons");
         manager.add("Europa Report");
-
-        listDataChild.put(listDataHeader.get(0), gardening); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), cleaning);
-        listDataChild.put(listDataHeader.get(2), security);
-        listDataChild.put(listDataHeader.get(3), door);
-        listDataChild.put(listDataHeader.get(4), services);
-        listDataChild.put(listDataHeader.get(5), manager);
     }
 
 }
